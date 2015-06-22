a494eb393c03770c3afe344bdf04b1c4b1fcaa04
Kondal Rao Komaragiri <kondal04@gmail.com>
2015-06-22 16:45:18 -0700

    Updateing the site url

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Kondal Rao Komaragiri'
 SITENAME = u'Build Crash Repeat'
-SITEURL = ''
+SITEURL = 'http://kondalrao.github.io/Build-Crash-Repeat'
 
 PATH = 'content'
 
